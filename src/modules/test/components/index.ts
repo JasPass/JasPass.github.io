90179baa20c8743adebb4327427af1211a97723c
JasPass <rasmusskn@gmail.com>
2021-08-21 22:12:38 +0200

    Initial commit

---
+++ src/modules/test/components/index.ts
@@ -0,0 +1,5 @@
+import { TestComponent } from './test/test.component';
+
+export const components = [TestComponent];
+
+export * from './test/test.component';
---
+++ src/modules/test/containers/index.ts
@@ -0,0 +1,5 @@
+import { TestComponent } from './test/test.component';
+
+export const containers = [TestComponent];
+
+export * from './test/test.component';
